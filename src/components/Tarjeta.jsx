8d71d8bcd1e5722577457ba6856ab68bd6fb6738
David Cortés <daviidcl11@gmail.com>
2024-09-15 03:00:15 +0200

    card images

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -2,6 +2,7 @@ export const Tarjeta = () => {
   return (
         <div className="card">
           <div className="card-info">
+            <div className="image-card"></div>
             <p className="title">Bienvenido</p>
           </div>
         </div>
