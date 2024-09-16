f14a3ec0a86393c70e10dd3089f3eeb5997746aa
David Cortés <daviidcl11@gmail.com>
2024-09-16 13:18:30 +0200

    changes

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -3,7 +3,8 @@ export const Tarjeta = () => {
         <div className="card">
           <div className="card-info">
             <div className="image-card"></div>
-            <p className="title">Bienvenido</p>
+            <p className="title">Lorem ipsum</p>
+            <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ullam sapiente aliquid vitae illo ea nam temporibu.</p>
           </div>
         </div>
   )
